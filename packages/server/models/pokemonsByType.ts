0bc314d61e8d586a962d6910f74ae738165b2139
Alessandro Lelli <alessandro.lelli.external@ynap.com>
2021-09-16 00:59:06 +0200

    initial

---
+++ packages/server/models/pokemonsByType.ts
@@ -0,0 +1,31 @@
+import { pipe } from "fp-ts/lib/pipeable";
+import { identity } from "fp-ts/lib/function";
+import { data } from "../data/pokemons";
+import { toConnection } from "../functions";
+import { Connection } from "../types";
+
+interface Pokemon {
+  id: string;
+  name: string;
+  types: string[];
+}
+
+export function query(args: {
+  type: string;
+}): Connection<Pokemon> {
+  const { type } = args;
+
+  const filterByType: (as: Pokemon[]) => Pokemon[] =
+    type === undefined
+      ? identity
+      : as =>
+        pipe(
+          as.filter(a => a.types.includes(type))
+        );
+
+  const results: Pokemon[] = pipe(
+    data,
+    filterByType
+  );
+  return toConnection(results, Infinity);
+}
